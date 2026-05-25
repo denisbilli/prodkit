89710cb154bbda1970bc5ca38e6ca21023741406
Denis Billi <denis.billi@scmgroup.com>
2026-05-25 18:05:29 +0200

    feat: expand product expectation types for enhanced capability evaluation

--- src/api.ts
+++ src/api.ts
@@ -7,7 +7,13 @@ export { renderMarkdownPlan } from './planner/markdownPlan';
 export { renderJsonPlan } from './planner/jsonPlan';
 
 export type { ProjectAnalysis, StackInfo, DetectorEvidence } from './analyzer/types';
-export type { ProductProfile, ProductExpectationResult } from './expectations/types';
+export type {
+	ProductProfile,
+	ProductExpectationResult,
+	CapabilityEvaluation,
+	CapabilityStatus,
+	CapabilityImportance,
+} from './expectations/types';
 export type { ProductionReadinessReport, Finding, MaturityLevel } from './report/types';
 export type { RemediationPlan, RemediationTask, RemediationPhase } from './planner/types';
 export type { BuildReportOptions } from './report/buildReport';
